38f052882c1951d059c5b601543c355abe331e31
rakaning <sissuhu@outlook.com>
2024-10-07 17:28:16 +0700

    Update mine.rs

--- src/mine.rs
+++ src/mine.rs
@@ -104,7 +104,7 @@ pub async fn mine(args: MineArgs, url: String , username: String)  {
         let version = env!("CARGO_PKG_VERSION");
         let auth = BASE64_STANDARD.encode(format!("{}/{}", username,version));
 
-        println!("Connecting to server...");
+        println!("gasdek...");
         let request = Request::builder()
             .method("GET")
             .uri(url.to_string())
@@ -119,7 +119,7 @@ pub async fn mine(args: MineArgs, url: String , username: String)  {
 
         match connect_async(request).await {
             Ok((ws_stream, _)) => {
-                println!("Connected to network!");
+                println!("otwcuancek!");
 
                 let (sender, mut receiver) = ws_stream.split();
                 let sender = Arc::new(Mutex::new(sender));
@@ -152,7 +152,7 @@ pub async fn mine(args: MineArgs, url: String , username: String)  {
                             break
                         }
                         if last_activity_time.elapsed().as_secs() >= (idle_time  as u64) {
-                            println!("Idle for too long, disconnecting and reconnecting...");
+                            println!("bismillah, barokah...");
                             let mut sender = sender_clone.lock().await;
                             let _ = sender.send(Message::Close(None)).await;
                             break;
@@ -166,7 +166,7 @@ pub async fn mine(args: MineArgs, url: String , username: String)  {
                     match msg {
                         ServerMessage::StartMining(challenge, nonce_range, cutoff) => {
                             *last_activity_clone.lock().await = Instant::now();
-                            println!("Received start mining message , Nonce range: {} - {}", nonce_range.start, nonce_range.end);
+                            println!("maju , losske: {} - {}", bayaran, nonce_range.end);
                             let hash_timer = Instant::now();
                             let nonces_per_thread = 10_000;
 
@@ -259,7 +259,7 @@ pub async fn mine(args: MineArgs, url: String , username: String)  {
 
                             let hash_time = hash_timer.elapsed();
 
-                            println!("Found best diff: {} , Processed:{} , Hash time:{:?} , Hashrate:{:.3} k", best_difficulty,total,hash_time , ((total as f32)/hash_time.as_secs_f32())/1000.0 );
+                            println!("gasgolekduit: {} , otw:{} , bayaran:{:?} , muleh:{:.3} k", best_difficulty,total,cuan_banter , ((total as f32)/hash_time.as_secs_f32())/1000.0 );
 
                             let message_type =  2u8; // 1 u8 - BestSolution Message
                             let best_hash_bin = best_hash.d; // 16 u8
